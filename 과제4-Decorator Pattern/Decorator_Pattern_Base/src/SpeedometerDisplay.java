44e835b2a125d2f42c59fd8bea81f80c0283ce18
Sugeun <songsurl@naver.com>
2022-12-01 15:54:52 +0900

    과제4 - Decorator Pattern - Base 버전

---
+++ 과제4-Decorator Pattern/Decorator_Pattern_Base/src/SpeedometerDisplay.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SpeedometerDisplay extends BaseDisplayDecorator {
+    public SpeedometerDisplay(Display display, int width, int height) {
+        super(display, width, height);
+    }
+
+    @Override
+    public String getText() {
+        return "Speed: 50";
+    }
+}
---
+++ 과제4-Decorator Pattern/Decorator_Pattern_Base/src/TimeDisplay.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TimeDisplay extends BaseDisplayDecorator {
+    public TimeDisplay(Display display, int width, int height) {
+        super(display, width, height);
+    }
+
+    @Override
+    public String getText() {
+        java.time.LocalDateTime t = java.time.LocalDateTime.now();
+        return "Date: " + t.toString();
+    }
+}
