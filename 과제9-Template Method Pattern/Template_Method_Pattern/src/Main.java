44504dcb0e2e75b0235015642d257d543454ec70
Sugeun <songsurl@naver.com>
2022-12-06 14:52:14 +0900

    과제 9 - main 수정

--- 과제9-Template Method Pattern/Template_Method_Pattern/src/Main.java
+++ 과제9-Template Method Pattern/Template_Method_Pattern/src/Main.java
@@ -3,11 +3,8 @@ public class Main {
         final String HOUSE = "house.png";
         String[] prefixes = {"g_", "i_", "s_"};
 
-        ImgConverter[] converters = new ImgConverter[3];
-        converters[0] = new GreyImageConverter();
-        converters[1] = new InverseImageConverter();
-        converters[2] = new SepiaImageConverter();
-        for (int i = 0; i < 3; i++) {
+        ImgConverter[] converters = {new GreyImageConverter(), new InverseImageConverter(), new SepiaImageConverter()};
+        for (int i = 0; i < converters.length; i++) {
             converters[i].convert(HOUSE, prefixes[i]);
         }
     }
